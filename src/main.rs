d71b6217cb99a3622c91d9bcc39c15b1c82bb3df
Tiim <tim.bachmann96@gmail.com>
2023-03-02 00:04:36 +0100

    feat: correctly display normal emojis

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use std::time::Duration;
-use colored::{Colorize, ColoredString};
+use colored::Colorize;
 
 use tokio::{task, time};
-use youtube_chat::{live_chat::LiveChatClientBuilder, item::{ChatItem, MessageItem::Text, MessageItem::Emoji}};
+use youtube_chat::{live_chat::LiveChatClientBuilder, item::{ChatItem, MessageItem::Text, MessageItem::Emoji, EmojiItem}};
+use demoji::demoji;
 
 #[tokio::main]
 async fn main() {
     let mut client = LiveChatClientBuilder::new()
-        .url("https://www.youtube.com/watch?v=iLqlRq1Kmg4".to_string())
+        .url("https://www.youtube.com/watch?v=xOw4_GRDqQE".to_string())
         .unwrap()
         .on_chat(|chat_item|print_chat_message(chat_item))
         .on_error(|error| eprintln!("{:?}", error))
@@ -29,19 +30,43 @@ fn print_chat_message(msg: ChatItem) {
     let author = msg.author;
     let txt = msg.message;
 
-    let obracket = "<".green();
-    let cbracket = ">".green();
+    let bar = "|".truecolor(100,100,100);
 
     if let Some(author_name) = author.name {
-        print!("{}{}{} ", obracket, author_name.cyan(), cbracket);
+        print!("{:<16.16}{}", demoji(&author_name).green().bold(), bar);
     }
     let full_text = txt
         .iter()
         .map(|t| {
             match t {
-                Text(s) => s,
-                Emoji(_) => "", 
+                Text(s) => s.clone(),
+                Emoji(emoji) => emoji_to_text(emoji), 
             }
-        }).fold(String::new(), |a,b| a+b+"\n");
-    print!("{}", full_text.blue());
+        }).fold(String::new(), |a,b| {
+            if b.trim().len() == 0 {
+                a
+            } else {
+                a+&b
+            }
+        });
+    print!("{}\n", full_text.blue());
 }
+
+fn emoji_to_text(emoji: &EmojiItem) -> String {
+    let mut text = String::new();
+    if let Some(emj_text) = emoji.emoji_text.clone() {
+        text.push_str(&emj_text);
+    }
+    else if let Some(img) = emoji.image_item.clone() {
+        if let Some(alt) = img.alt {
+            text.push_str(&alt);
+        }
+    }
+    text
+}
+
+
+
+
+
+
